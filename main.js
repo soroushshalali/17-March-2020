fdc84d13eb5d52693285a431ec3ac23100f57e54
soroushshalali <soroushshalali@gmail.com>
2020-03-17 12:38:43 +0100

    v0.1

---
+++ main.js
@@ -0,0 +1,30 @@
+// console.log(Math.random()) // 0 =>1
+// console.log(Math.PI) //3.14
+// console.log(Math.E) //2.71
+// console.log(Math.round(3.49)) //3
+// console.log(Math.round(6.5))
+// console.log(Math.round(3.6)) // 4
+// console.log(Math.floor(9.99))//9
+// console.log(Math.ceil(11.0000001))//12
+// console.log(Math.trunc(43.87787687))
+// console.log(Math.abs(-34))
+
+// let d = new Date()
+// console.log(d)
+// console.log(d.getFullYear()) //2020
+// console.log(d.getHours())
+// console.log(d.getSeconds())
+
+// let birthday = new Date(1993, 0, 3, 11, 50, 10)
+// let birthday1 = new Date(1993, 0, 3, 11, 50, 10)
+// console.log(birthday.getTime())
+// //Returns the number of milliseconds since midnight Jan 1 1970, and a specified date
+// if (birthday.getTime() == birthday1.getTime()) {
+//     console.log('gleich')
+// } else {
+//     console.log("not gleich")
+// }
+
+// //setInterval and setTimeout
+
+// console.log(Math.floor(Math.random() * 100))
